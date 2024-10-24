903a69764263811ea55b3022bf6c74538852e970
Patrik Rufino <58515198+patrikrufino@users.noreply.github.com>
2024-10-24 11:30:59 -0300

    Adiciona sidebar com tags, navegação, paginação e pesquisa

--- back-django/core/urls.py
+++ back-django/core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
-from .views import blog_view
+from .views import blog_view, post_detail_view
 
 urlpatterns: list[URLPattern] = [
     path('blog/', blog_view, name='blog'),
+    path('blog/<int:post_id>/', post_detail_view, name='post_detail'),
 ]
--- back-django/core/views.py
+++ back-django/core/views.py
@@ -1,8 +1,40 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.db.models.manager import BaseManager
 from django.http import HttpResponse
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Tag
 
 def blog_view(request) -> HttpResponse:
     posts: BaseManager[Post] = Post.objects.all()
-    return render(request, 'core/blog.html', {'posts': posts})
+    tags = Tag.objects.all()
+    print(tags)
+
+    # Verificar se uma tag foi selecionada
+    selected_tag = request.GET.get('tag')
+    if selected_tag:
+        posts = posts.filter(tags__name=selected_tag)
+
+    # Verificar se há uma pesquisa
+    search_query = request.GET.get('search')
+    if search_query:
+        posts = posts.filter(title__icontains=search_query)
+
+    # Configurar paginação com 5 itens por página
+    paginator = Paginator(posts, 5)
+    page = request.GET.get('page')
+
+    try:
+        paginated_posts = paginator.page(page)
+    except PageNotAnInteger:
+        paginated_posts = paginator.page(1)
+    except EmptyPage:
+        paginated_posts = paginator.page(paginator.num_pages)
+
+    return render(request, 'core/blog.html', {
+        'posts': paginated_posts,
+        'tags': tags
+    })
+
+def post_detail_view(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    return render(request, 'core/post_detail.html', {'post': post})
